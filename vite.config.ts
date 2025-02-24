72c385c49b9efcded21ff10c944d51f132f82713
robinerol <robin.stuedle@gmail.com>
2025-02-24 17:47:50 +0100

    add autoprefixer for better cross browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     outDir: 'dist',
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
   },
   base: '/deck-view/',
 });
